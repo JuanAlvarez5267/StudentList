156c835ae9a387399f782e043ddd8992476913a7
Juan Alvarez <alvarez1@sheridancollege.ca>
2023-05-16 08:59:24 -0500

    First Comit

---
+++ src/studentlist/Student.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package studentlist;
+
+
+public class Student {
+    
+    private String sname;
+    private String SID;
+
+    public Student(String sname, String SID) {
+        this.sname = sname;
+        this.SID = SID;
+    }
+    
+    
+
+    public String getSname() {
+        return sname;
+    }
+
+    public void setSname(String sname) {
+        this.sname = sname;
+    }
+
+    public String getSID() {
+        return SID;
+    }
+
+    public void setSID(String SID) {
+        this.SID = SID;
+    }
+    
+    
+    
+}
---
+++ src/studentlist/StudentList.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package studentlist;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Juan Alvarez
+ */
+public class StudentList {
+
+    public static void main(String[] args) {
+      Student s1 = new Student("Ronak", "s1");
+      //Scanner input = new Scanner(System.in);
+      Student[] studentList = new Student[3];
+      studentList[0]=s1;
+      studentList[1]= new Student("abc", "s2");
+      studentList[2]= new Student("def", "s3");
+      
+      for(int i = 0; i<studentList.length; i++){
+          System.out.println(studentList[i].getSname());
+      }
+      
+    }
+    
+}
